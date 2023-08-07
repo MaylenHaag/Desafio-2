646b864a00417bcab3419ef47292d33e1fab940a
MaylenHaag <maylenhaag23@gmail.com>
2023-08-07 12:01:27 -0300

    entrega lista

--- ProductManager.js
+++ ProductManager.js
@@ -14,7 +14,7 @@ class ProductManager {
         }
     }
 
-    addProduct(product) {
+    async addProduct(product) {
 
         const requiredFields = ["title", "description", "price", "thumbnail", "code", "stock"];
 
@@ -24,6 +24,10 @@ class ProductManager {
             return 'Todos los campos son obligatorios'
         }
 
+        let data = await fs.promises.readFile(this.#path, 'utf-8')
+
+        let products = JSON.parse(data)
+
         const found = this.#products.find(item => item.code === product.code)
 
         if (found) {
@@ -32,6 +36,9 @@ class ProductManager {
 
         const productToAdd = {id: this.#generateId(), ...product}
         this.#products.push(productToAdd)
+
+        await fs.promises.writeFile(this.#path, JSON.stringify(products, null, 2))
+
         return productToAdd
 
     }
@@ -43,18 +50,94 @@ class ProductManager {
         return this.#products[this.#products.length-1].id + 1
     }
 
-    getProducts() {
-        return this.#products
+    async getProducts() {
+        if (!fs.existsSync(this.#path)) {
+            return 'El archivo no existe en la base de datos.'
+        }
+
+        let data = await fs.promises.readFile(this.#path, 'utf-8')
+
+        const products = JSON.parse(data)
+
+        return products
     }
 
-    getProductById(id) {
-        const found = this.#products.find(item => item.id === id)
+    async getProductById(id) {
+        if (!fs.existsSync(this.#path)) {
+            return 'El archivo no existe en la base de datos.'
+        }
 
-        if (!found) {
+        let product = products.find(item => item.id === id)
+
+        let data = await fs.promises.readFile(this.#path, 'utf-8')
+
+        let products = JSON.parse(data)
+
+        if (!product) {
             return 'Id no encontrado.'
         }
 
-        return found
+        return product
+    }
+
+    async updateProduct(id, updateProduct) {
+        if (!fs.existsSync(this.#path)) {
+            return 'El archivo no existe en la base de datos.'
+        }
+
+        let isFound = false
+
+        let data = await fs.promises.readFile(this.#path, 'utf-8')
+
+        let products = JSON.parse(data)
+
+        let newProducts = products.map(item => {
+            if (item.id === id) {
+                isFound = true
+
+                return {
+                    ...item,
+                    ...updateProduct
+                }
+            } else {
+                return item
+            }
+        })
+
+        if (!isFound) {
+            return 'El rpducto no existe.'
+        }
+
+        await fs.promises.writeFile(this.#path, JSON.stringify(newProducts, null, 2))
+
+        return newProducts.find(item => item.id === id)
+    }
+
+    async deleteProduct(id) {
+        if (!fs.existsSync(this.#path)) {
+            return 'El archivo no existe en la base de datos.'
+        }
+
+        let isFound = false
+
+        let data = await fs.promises.readFile(this.#path, 'utf-8')
+
+        let products = JSON.parse(data)
+
+        let newProducts = products.filter(item => item.id !== id)
+
+        if (products.length !== newProducts.length) {
+            isFound = true
+        }
+
+        if (!isFound) {
+            return 'El rpducto no existe.'
+        }
+
+        await fs.promises.writeFile(this.#path, JSON.stringify(newProducts, null, 2))
+        
+        return newProducts
+
     }
 }
 
